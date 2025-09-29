371566f6422747b1692177d1bedda8adbb1b64ef
Mahesh-619Zeno <mahesh.ghadei@techolution.com>
2025-09-29 17:40:26 +0530

    refectored and updated the code

--- PrimeChecker.java
+++ PrimeChecker.java
@@ -14,3 +14,18 @@ public class PrimeChecker {
 
             for (int i = 2; i <= Math.sqrt(number); i++) {
                 if (number % i == 0) {
+                    isPrime = false;
+                    break;  
+                }
+            }
+
+            if (isPrime) {
+                System.out.println(number + " is a prime number.");
+            } else {
+                System.out.println(number + " is not a prime number.");
+            }
+        }
+
+        scanner.close();
+    }
+}
